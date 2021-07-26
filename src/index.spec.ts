c6aeecb9b08558ef27915b42dcfdda22a43d4205
DeruiDENG <moonface.ddr@gmail.com>
2021-07-26 13:58:40 +0800

    Send simple move forward command.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -9,16 +9,29 @@ class Rover {
     this.direction = direction;
   }
 
-  getX() {
-    return this.x;
-  }
-
-  getY() {
-    return this.y;
-  }
-
   getDirection() {
     return this.direction;
+  }
+
+  getPosition() {
+    return { x: this.x, y: this.y };
+  }
+
+  private forward() {
+    if (this.direction === Direction.North) {
+      this.y--;
+    }
+  }
+
+  sendCommand(command: Command) {
+    switch (command) {
+      case Command.Forward: {
+        this.forward();
+        break;
+      }
+      default:
+        throw new Error(`Unsupported command: ${command}`);
+    }
   }
 }
 
@@ -29,11 +42,21 @@ enum Direction {
   East,
 }
 
+enum Command {
+  Forward = "f",
+}
+
 describe("#Mars", () => {
   it("should put the rover to a place", () => {
     const rover = new Rover(1, 2, Direction.North);
-    expect(rover.getX()).toEqual(1);
-    expect(rover.getY()).toEqual(2);
+    expect(rover.getPosition()).toEqual({ x: 1, y: 2 });
+    expect(rover.getDirection()).toEqual(Direction.North);
+  });
+
+  it("should command the rover with forward", () => {
+    const rover = new Rover(1, 2, Direction.North);
+    rover.sendCommand(Command.Forward);
+    expect(rover.getPosition()).toEqual({ x: 1, y: 1 });
     expect(rover.getDirection()).toEqual(Direction.North);
   });
 });
