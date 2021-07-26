97047808699b9144f7a78041826f78bc2f1dbcbe
DeruiDENG <moonface.ddr@gmail.com>
2021-07-26 13:36:53 +0800

    Put the rover into the Mars.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,3 +1,39 @@
-it("xxx", () => {
-	expect(true).toEqual(true);
-})
+class Rover {
+  private x: number;
+  private y: number;
+  private direction: Direction;
+
+  constructor(x: number, y: number, direction: Direction) {
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
+  }
+
+  getX() {
+    return this.x;
+  }
+
+  getY() {
+    return this.y;
+  }
+
+  getDirection() {
+    return this.direction;
+  }
+}
+
+enum Direction {
+  North,
+  South,
+  West,
+  East,
+}
+
+describe("#Mars", () => {
+  it("should put the rover to a place", () => {
+    const rover = new Rover(1, 2, Direction.North);
+    expect(rover.getX()).toEqual(1);
+    expect(rover.getY()).toEqual(2);
+    expect(rover.getDirection()).toEqual(Direction.North);
+  });
+});
